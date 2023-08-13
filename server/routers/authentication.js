84973f5b6d01a3b8c0f4f868cf7e147fec3dd62a
a-riceeater <elijb12@gmail.com>
2023-08-12 23:25:11 -0700

    Backend

--- server/routers/authentication.js
+++ server/routers/authentication.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const databaseHandler = require("jdb");
 
+const accountDb = databaseHandler.database(path.join(__dirname, "../", "../", "database", "accounts.json"))
 const app = express.Router();
 
 app.get("/start", (req, res) => {
@@ -9,7 +10,20 @@ app.get("/start", (req, res) => {
 })
 
 app.post("/create-account", (req, res) => {
+    const info = req.body;
     
+    if (info.email.trim() == "") return res.send({ error: true })
+    if (info.username.trim() == "") return res.send({ error: true })
+    if (info.password.trim() == "") return res.send({ error: true })
+
+
+    if (accountDb.getRowSync("accounts", "email", info.email)) {
+        res.send({
+            error: true
+        })
+    } else {
+        
+    }
 })
 
 module.exports = app;
