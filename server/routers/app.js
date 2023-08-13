cad89a153a950d5687e208e597d8a79a01acbf1a
a-riceeater <elijb12@gmail.com>
2023-08-13 00:37:56 -0700

    page data

--- server/routers/app.js
+++ server/routers/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require("path");
 const fs = require("fs");
+const middle = require("../middleware")
 
 const app = express.Router();
 
-app.get("/self", (req, res) => {
-    res.send(200);
+app.get("/self", middle.authenticateToken, (req, res) => {
+    const data = fs.readFileSync(path.join(__dirname, "../../", "html", "app", "self.html"), "utf8")
+        .replaceAll("{{ username }}", res.user)
+        .replaceAll("{{ email }}", res.email);
+    
+    res.send(data);
 })
 
 module.exports = app;
