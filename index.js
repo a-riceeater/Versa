78ab9cb3aa4f1f26b650046550ed766c761e4380
Elijah <46640763+a-riceeater@users.noreply.github.com>
2023-08-16 20:24:29 +0000

    cdn api

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const path = require("path");
 const databaseHandler = require("jdb");
 const favicon = require('serve-favicon');
 const cors = require("cors");
+const middle = require("./server/middleware")
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "static")));
 app.use(express.json())
 app.use("/cdn/", express.static(path.join(__dirname, "cdn")));
+app.use("/cdn/", middle.authenticateToken);
 
 app.use("/app/", require(path.join(__dirname, "server", "routers", "app.js")));
-app.use("/auth/", require(path.join(__dirname, "server", "routers", "authentication.js")))
-app.use("/app-api/", require(path.join(__dirname, "server", "routers", "app-api.js")))
+app.use("/auth/", require(path.join(__dirname, "server", "routers", "authentication.js")));
+app.use("/app-api/", require(path.join(__dirname, "server", "routers", "app-api.js")));
+app.use("/cdn-api/", require(path.join(__dirname, "server", "cdn.js")));
 
 app.use(favicon(path.join(__dirname, 'static', 'versa.png')));
 
---
+++ server/cdn.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const dbInstances = require("./dbInstances"); // possibly not needed, since it writes directly, but it may require token validation?
+const middle = require("./middleware");
+
+const app = express.Router();
+
+app.post("/upload-file", middle.authenticateToken, (req, res) => {
+})
