5100a9d868983bceb6b202f494b3537f436b361e
Elijah <46640763+a-riceeater@users.noreply.github.com>
2023-08-16 20:30:24 +0000

    multer npm

--- server/cdn.js
+++ server/cdn.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const dbInstances = require("./dbInstances"); // possibly not needed, since it writes directly, but it may require token validation?
 const middle = require("./middleware");
+const multer = require('multer');
 
 const app = express.Router();
 
