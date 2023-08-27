e9a04b4f6b8f3af88f470d3f3e343663533f1752
a-riceeater <elijb12@gmail.com>
2023-08-27 00:32:52 -0700

    Accept btn

--- server/routers/app.js
+++ server/routers/app.js
@@ -59,7 +59,14 @@ app.get("/widget/KjitLwgKq6AjPyLi28BSy7SXQ", middle.authenticateToken, (req, res
     let pending = "";
 
     for (let i = 0; i < friendRow.pendingFrom.length; i++) {
-        pending += friendRow.pendingFrom[i].username;
+        pending += `
+        <div class="scb-frmo-frbtn">
+        <span class="title">${friendRow.pendingFrom[i].username}</span>
+        <span class="desc">Incoming Friend Request</span>
+        <button class="accept-fr">
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+        </button>
+        </div>`
     }
 
     for (let i = 0; i < friendRow.pendingTo.length; i++) {
