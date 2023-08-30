4ff56226166cb4ec35536b6be001efa95098dc8a
Elijah <46640763+a-riceeater@users.noreply.github.com>
2023-08-30 17:38:42 +0000

    contextmenu base

---
+++ static/app/context-menu.js
@@ -0,0 +1,17 @@
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+
+    switch (e.target.getAttribute("data-context-id")) {
+        case "friend-button":
+
+    }
+})
+
+/**
+ * The class to create a custom context menu
+ */
+class ContextMenu {
+    constructor (options) {
+
+    }
+}
