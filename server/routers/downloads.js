7fe1fe259c14502112d82e64aae50b0e1a2d2a61
darthvader1925 <elijb12@gmail.com>
2023-09-07 17:43:37 -0700

    download

--- server/routers/downloads.js
+++ server/routers/downloads.js
@@ -2,17 +2,18 @@ const express = require("express");
 const path = require("path");
 
 const app = express.Router();
+const version = JSON.parse(require("fs").readFileSync(path.join(__dirname, "../", "../", "current_version.json"), "utf8")).version;
 
 app.get("/windows", (req, res) => {
-    
+    res.download(path.join(__dirname, "../", "../", "app", "dist", "Versa-" + version + ".exe"));
 })
 
 app.get("/macos", (req, res) => {
-    res.sendFile(path.join(__dirname, "../", "../", "app", "dist"))
+    res.download(path.join(__dirname, "../", "../", "app", "dist", "Versa-" + version + ".dmg"));
 })
 
 app.get("linux", (req, res) => {
-    
+    res.download(path.join(__dirname, "../", "../", "app", "dist", "Versa-" + version + ".dmg"));
 })
 
 module.exports = app;
