11e5b60a3f2442439e1d564397db2a0f9e43dd29
Elijah <46640763+a-riceeater@users.noreply.github.com>
2023-09-11 17:29:34 +0000

    message content base

--- server/routers/app.js
+++ server/routers/app.js
@@ -228,7 +228,16 @@ app.get("/chat/dm/:chatId", middle.authenticateToken, (req ,res) => {
         <button class="cmfrb-dft block-fr">Block</button>
 
         <div class="cm-divider"></div>
-        <div class="cm-msg-history"></div>
+        <div class="cm-msg-history">
+            <div class="cm-mb-mse" data-mid="asdasd" data-fuser="a#6419" data-fid="asfhisdff">
+                <div class="cm-mb-mtt">
+                    <svg class="cmb-mt-bo delete" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
+                </div>
+                <div class="cm-mb-mcontent">
+                    wsg
+                </div>
+            </div>
+        </div>
 
         <div class="cm-ed-reply"></div>
         <div class="cm-editor">
