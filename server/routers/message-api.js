e3e8037380b67c6c0ffb9f0d1367d66c441e9d78
Elijah <46640763+a-riceeater@users.noreply.github.com>
2023-09-11 17:03:05 +0000

    rate limiter

--- server/routers/message-api.js
+++ server/routers/message-api.js
@@ -2,18 +2,33 @@ const express = require("express");
 const path = require("path");
 const dbInstances = require("../dbInstances");
 const middleware = require("../middleware");
-const rateLimit = require("express-rate-limit")
+const { rateLimit } = require('express-rate-limit')
 
 const app = express.Router();
 
 const socketIds = dbInstances.socketIds;
 const rooms = dbInstances.rooms;
 
-app.post("/send-message", middleware.authenticateToken, (req, res) => {
+const messageLimit = rateLimit({
+	windowMs: 5 * 1000, // 5 seconds
+	max: 10,
+	standardHeaders: true,
+	legacyHeaders: false, 
+    statusCode: 429,
+    message: {
+        error: "Too many messages",
+        limiter: true,
+        type: "error"
+    }
+})
+
+app.post("/send-message", (middleware.authenticateToken, messageLimit), (req, res) => {
     const message = req.body.message;
     const chatId = req.body.chatId;
     
     // determine if user has access to channel
+    
+    res.send({ sent: true })
 })
 
 module.exports = app;
